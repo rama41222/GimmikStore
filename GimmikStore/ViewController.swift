2a556012ed3f518791242d9f340b6974f75709bc
rama41222 <dinushankanrg@gmail.com>
2018-10-03 00:01:51 +0530

    initial commit

---
+++ GimmikStore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GimmikStore
+//
+//  Created by Dinushanka Nayomal on 10/2/18.
+//  Copyright © 2018 Viyana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# GimmikStore
+iOS App to search content from iTunes
