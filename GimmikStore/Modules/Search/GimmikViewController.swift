efd33034fe506f9b91a221d137ce5f9c1b64e6b1
rama41222 <dinushankanrg@gmail.com>
2018-10-03 02:13:10 +0530

    added main vc

---
+++ GimmikStore/Modules/Search/GimmikViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GimmikViewController.swift
+//  GimmikStore
+//
+//  Created by Dinushanka Nayomal on 10/3/18.
+//  Copyright © 2018 Viyana. All rights reserved.
+//
+
+import UIKit
+
+class GimmikViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
