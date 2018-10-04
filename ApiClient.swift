a3f23b28df14b78acced88809f3b9694deac9484
MayanthaJ <mayantha_j@yahoo.co.uk>
2018-10-04 21:38:53 +0530

    ApiClient response handler added

--- ApiClient.swift
+++ ApiClient.swift
@@ -7,13 +7,28 @@
 //
 
 import Foundation
-import Almofire
+import Alamofire
 
 class ApiClient {
     static func search(term: String, entity: String = "software", limit: Int = 2, completion:@escaping (GimmikResult?)->Void) {
         Alamofire.request(ApiRouter.search(term, entity, limit))
             .responseData { response in
                 print(response)
+                
+                guard response.result.isSuccess,
+                    let value = response.result.value else {
+                        print("Error while fetching Apps: \(String(describing: response.result.error))")
+                        completion(nil)
+                        return
+                }
+                do {
+                    let gimmikApps = try JSONDecoder().decode(GimmikResult.self, from: value)
+                    completion(gimmikApps)
+                } catch let error {
+                    print("Error while fetching Apps, JSON Parsing error: \(error)")
+                    completion(nil)
+                }
+
                 
         }
     }
