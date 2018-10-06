a6ba754d3d16afb155d654dc1d0bc2a7fa0dd146
rama41222 <dinushankanrg@gmail.com>
2018-10-06 16:16:08 +0530

    .

--- GimmikStore/Modules/Networking/ApiRouter.swift
+++ GimmikStore/Modules/Networking/ApiRouter.swift
@@ -23,18 +23,25 @@ enum ApiRouter: URLRequestConvertible {
     var location: String {
         switch self {
         case .search(let term, let entity, let limit):
-            return "/search?term=\(term)&entity=\(entity)&limit=\(limit)"
+            return "/search?term=\(encode(term))&entity=\(entity)&limit=\(limit)"
         }
+    }
+    
+    func encode(_ term: String) -> String {
+        let escapedAddress = term.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)
+        return escapedAddress ?? ""
     }
     
     func asURLRequest() throws -> URLRequest {
         let baseUrl = Config.BaseUrl
-        let url = URL(string: baseUrl.appending(location).removingPercentEncoding!)
+        let url = URL(string: baseUrl.appending(location))
         var request = URLRequest(url: url ?? URL(string: Config.generalUrl)!)
         
         request.httpMethod = method.rawValue
         request.setValue(ContentType.json.rawValue, forHTTPHeaderField: HTTPHeaderField.contentType.rawValue)
         request.timeoutInterval = TimeInterval(10 * 1000)
+        print(request)
+        print(try URLEncoding.default.encode(request, with: nil))
         return try URLEncoding.default.encode(request, with: nil)
         
     }
